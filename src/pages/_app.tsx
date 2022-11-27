1f4e20f12785d4ccc931c7b62079211bbb750619
gspidermac <gspidermac@gmail.com>
2022-11-27 22:10:08 +0100

    - Added Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/global.css';
 import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = ({ Component, pageProps }) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
@@ -26,6 +27,7 @@ const App = ({ Component, pageProps }) => {
       >
         <main className="bg-light-background dark:bg-dark-background w-full h-full p-2">
           <Component {...pageProps} inputRef={inputRef} />
+          <Analytics />
         </main>
       </div>
     </>
